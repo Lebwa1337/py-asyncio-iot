accc972dc897cecab7f262e9a8a6517d56bd0518
LeBwa <lebwa1337@gmail.com>
2024-04-24 22:20:31 +0300

    Change order of the send message function

--- app/iot/devices.py
+++ app/iot/devices.py
@@ -20,12 +20,11 @@ class HueLightDevice:
         print("Hue Light disconnected.")
 
     async def send_message(self, message_type: MessageType, data: str = "") -> None:
+        print("Hue Light received message.")
+        await asyncio.sleep(TIME_TO_SLEEP)
         print(
             f"Hue Light handling message of type {message_type.name} with data [{data}]."
         )
-        await asyncio.sleep(TIME_TO_SLEEP)
-        print("Hue Light received message.")
-
 
 class SmartSpeakerDevice:
     async def connect(self) -> None:
@@ -39,11 +38,11 @@ class SmartSpeakerDevice:
         print("Smart Speaker disconnected.")
 
     async def send_message(self, message_type: MessageType, data: str = "") -> None:
+        print("Smart Speaker received message.")
+        await asyncio.sleep(TIME_TO_SLEEP)
         print(
             f"Smart Speaker handling message of type {message_type.name} with data [{data}]."
         )
-        await asyncio.sleep(TIME_TO_SLEEP)
-        print("Smart Speaker received message.")
 
 
 class SmartToiletDevice:
@@ -58,8 +57,8 @@ class SmartToiletDevice:
         print("Smart Toilet disconnected.")
 
     async def send_message(self, message_type: MessageType, data: str = "") -> None:
+        print("Smart Toilet received message.")
+        await asyncio.sleep(TIME_TO_SLEEP)
         print(
             f"Smart Toilet handling message of type {message_type.name} with data [{data}]."
         )
-        await asyncio.sleep(TIME_TO_SLEEP)
-        print("Smart Toilet received message.")
